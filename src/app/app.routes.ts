86202fc199d4f56288528fb1a1b1d56a81385d2d
jose <joensis@gmail.com>
2025-04-09 11:39:41 +0200

    secciones acabadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { DefaultComponent } from './views/default/default.component';
+import { SpacesComponent } from './views/spaces/spaces.component';
+import { ComunicationComponent } from './views/comunication/comunication.component';
+import { FilesComponent } from './views/files/files.component';
+import { ConfigurationComponent } from './views/configuration/configuration.component';
+import { PrincipalComponent } from './views/principal/principal.component';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "main", component: PrincipalComponent,
+        children:[
+            {path:"spaces", component: SpacesComponent},
+            {path: "comunication", component:ComunicationComponent},
+            {path: "files", component:FilesComponent},
+            {path: "configuration", component:ConfigurationComponent},
+            {path:"default", component: DefaultComponent},
+        ]
+    },
+    {path:"", redirectTo:"/main/default", pathMatch:"full"}    /* cambiar a login */
+    
+    
+];
--- src/app/views/principal/principal.component.ts
+++ src/app/views/principal/principal.component.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { NavSpacesComponent } from '../../components/nav-spaces/nav-spaces.component';
 import { NavComunicationComponent } from "../../components/nav-comunication/nav-comunication.component";
 import { NavFilesComponent } from "../../components/nav-files/nav-files.component";
-import { ConfigurationComponent } from "../configuration/configuration.component";
+import { NavConfigurationComponent } from '../../components/nav-configuration/nav-configuration.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-principal',
-  imports: [CommonModule, ProfileComponent, NavSpacesComponent, NavComunicationComponent, NavFilesComponent, ConfigurationComponent],
+  imports: [CommonModule, ProfileComponent, NavSpacesComponent, NavComunicationComponent, 
+    NavFilesComponent, NavConfigurationComponent, RouterOutlet],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })
