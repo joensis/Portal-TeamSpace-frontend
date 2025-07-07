74d60ab7d487440fddd5812192e022bde7b5925e
jose <joensis@gmail.com>
2025-07-07 18:43:31 +0200

    actualizar login y registro

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import {FullCalendarModule} from '@fullCalendar/angular';
+import dayGridPlugin from '@fullCalendar/daygrid';
+import timeGridPlugin from '@fullCalendar/timegrid';
+import interactionPlugin from '@fullCalendar/interaction';
+import esLocale from '@fullcalendar/core/locales/es';
 
 @Component({
   selector: 'app-calendar',
@@ -8,5 +12,44 @@ import {FullCalendarModule} from '@fullCalendar/angular';
   styleUrl: './calendar.component.css'
 })
 export class CalendarComponent {
+
+  public events: any[] = []; 
+  public options: any; 
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.events = [
+
+    {
+      title: "Evento 2",
+      start: new Date(),
+      description: "evento diario"
+
+    }
+  ]
+
+    this.options= {
+    
+      plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+      defaultDate: new Date(),
+      locale: esLocale, 
+      headeToolbar: {
+        left: 'prev, next',
+        center: 'tittle',
+        right: 'dayGridMonth, timeGridWeek, timeGridDay'
+      }
+    }
+    
+  }
+
+  
+    
+
+    
+  
+  
+  
+  
 
 }
--- src/app/views/default/default.component.ts
+++ src/app/views/default/default.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Calendar } from '@fullcalendar/core/index.js';
+import { CalendarComponent } from '../../components/calendar/calendar.component';
 
 @Component({
   selector: 'app-default',
-  imports: [],
+  imports: [CalendarComponent],
   templateUrl: './default.component.html',
   styleUrl: './default.component.css'
 })
